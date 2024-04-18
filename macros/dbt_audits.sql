4fefc49d121c1cf3030a92da5c5915727a82d882
srecko.zajec@tokic.hr <srecko.zajec@tokic.hr>
2024-04-18 23:55:15 +0000

    set if etc

--- macros/dbt_audits.sql
+++ macros/dbt_audits.sql
@@ -1,17 +1,44 @@
 {% macro audit_log(run_activity) %}
-insert into analytics_015.utilities.dbt_audit 
+
+{% set audit_schema = 'utilities' %}
+{% set audit_table = 'dbt_audit' %}
+
+{% set audit_query %}
+
+{% if this.name %}
+insert into {{target.database}}.{{audit_schema}}.{{audit_table}}
 values ('{{ invocation_id }}', '{{ this.name }}', '{{ run_activity }}', current_timestamp());
+
+{% else %}
+insert into {{target.database}}.{{audit_schema}}.{{audit_table}}
+values ('{{ invocation_id }}', 'N/A', '{{ run_activity }}', current_timestamp());
+
+{% endif %}
+
+{% endset %}
+
+{% do run_query (audit_query) %}
 
 {% endmacro %}
 
 
+
+
 {% macro audit_prep() %}
-create table if not exists analytics_015.utilities.dbt_audit 
+
+{% set audit_schema = 'utilities' %}
+{% set audit_table = 'dbt_audit' %}
+
+{% set audit_prep_query %}
+create table if not exists analytics_015.{{audit_schema}}.{{audit_table}}
 (
     run_id text,
     run_object text,
     run_activity text,
     created_at timestamp_ntz
 );
+{% endset %}
+
+{% do run_query (audit_prep_query) %}
 
 {% endmacro %}
